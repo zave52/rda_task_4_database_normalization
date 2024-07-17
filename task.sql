dc0f50e1859c9fb936544211e20971f331f0017b
zakhar <zaharsavchen@gmail.com>
2024-07-17 17:56:18 +0300

    fix warehouses and productinventory tables

--- task.sql
+++ task.sql
@@ -19,6 +19,8 @@ CREATE TABLE Warehouses (
     ID INT AUTO_INCREMENT,
     Name VARCHAR(50),
     Address VARCHAR(50),
+    CountryID INT,
+	FOREIGN KEY (CountryID) REFERENCES Countries(ID) ON DELETE NO ACTION,
     PRIMARY KEY (ID)
 );
 
@@ -28,8 +30,6 @@ CREATE TABLE ProductInventory (
     WarehouseID INT,
     FOREIGN KEY (WarehouseID) REFERENCES Warehouses(ID) ON DELETE NO ACTION,
     WarehouseAmount INT,
-    CountryID INT,
-	FOREIGN KEY (CountryID) REFERENCES Countries(ID) ON DELETE NO ACTION,
     PRIMARY KEY (ID)
 );
 
@@ -42,10 +42,10 @@ INSERT INTO Countries (ID,Name)
 INSERT INTO Products (ID, Name)
     VALUES (1, 'AwesomeProduct');
 
-INSERT INTO Warehouses (ID, Name, Address)
-    VALUES (1, 'Warehouse-1', 'City-1, Street-1'),
-           (2, 'Warehouse-2', 'City-2, Street-2');
+INSERT INTO Warehouses (ID, Name, Address, CountryID)
+    VALUES (1, 'Warehouse-1', 'City-1, Street-1', 1),
+           (2, 'Warehouse-2', 'City-2, Street-2', 2);
 
-INSERT INTO ProductInventory (ID, ProductID, WarehouseID, WarehouseAmount, CountryID)
-    VALUES (1, 1, 1, 2, 1),
-           (2, 1, 2, 5, 2);
+INSERT INTO ProductInventory (ID, ProductID, WarehouseID, WarehouseAmount)
+    VALUES (1, 1, 1, 2),
+           (2, 1, 2, 5);
